31d8e2b96405400ec3aff5afbe5c95ecfa97f5be
reggies-kagecha <54356670+reggies-kagecha@users.noreply.github.com>
2020-03-03 01:41:18 +0300

    Android VideoPlayer

---
+++ VideoPlayer/app/src/main/java/com/mwas/reggie/videoplayer/MainActivity.java
@@ -0,0 +1,41 @@
+package com.mwas.reggie.videoplayer;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        VideoView videoView =(VideoView)findViewById(R.id.videoView1);
+
+        //Creating MediaController
+        MediaController mediaController= new MediaController(this);
+        mediaController.setAnchorView(videoView);
+
+        //specify the location of media file
+        Uri uri=Uri.parse(Environment.getExternalStorageDirectory().getPath()+"/media/1.mp4");
+
+        //Setting MediaController and URI, then starting the videoView
+        videoView.setMediaController(mediaController);
+        videoView.setVideoURI(uri);
+        videoView.requestFocus();
+        videoView.start();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+}
